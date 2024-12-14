e80c9c8c108d3d301cc3524b9f25c9ceca5842d5
viola30th <viola30th@gmail.com>
2024-12-14 18:06:09 +0900

    AK-278 [Feat] 블로그 화면 반응형 추가

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,19 +8,22 @@ import axios from "../api/axios";
 const Profile = () => {
   const navigate = useNavigate();
   const writePage = () => {
-    navigate("/blog-service/api/posts");
+    navigate("/blog-service/auth/api/posts");
   };
   const goLoginPage = () => {
     navigate("/login");
   };
   const [profileData, setProfileData] = useState(null);
-  const { isAuthenticated, nickname: userNickname } = useRecoilValue(loginState);
+  const { isAuthenticated, nickname: userNickname } =
+    useRecoilValue(loginState);
   const { userId: userId } = useRecoilValue(loginState);
 
   useEffect(() => {
     const fetchTags = async () => {
       try {
-        const response = await axios.get(`/user-service/users/${userId}/profile`);
+        const response = await axios.get(
+          `/user-service/users/${userId}/profile`
+        );
         if (response.status === 200) {
           setProfileData(response.data.data); // API에서 가져온 태그 데이터를 상태에 저장
           console.log(response.data.data);
@@ -35,12 +38,12 @@ const Profile = () => {
     if (userId) {
       fetchTags();
     }
-  }, [userId]); 
+  }, [userId]);
 
   // 프로필 정보 로딩 중인 경우의 UI
   if (!profileData) {
     return (
-      <div className="w-[20%] h-screen px-12 py-12">
+      <div className="w-[20vw] h-screen px-12 py-12">
         <div className="border-[0.1px] border-gray-400 shadow-md rounded-2xl h-[90%] p-4 mt-16 text-center">
           <p className="text-gray-500">프로필 정보를 불러오는 중입니다...</p>
         </div>
@@ -54,11 +57,11 @@ const Profile = () => {
         // 로그인 상태이고, 본인의 프로필인 경우
         return (
           <>
-            <button className="text-black border-[1px] border-black rounded-full px-4 py-1 font-semibold">
+            <button className="text-black border-[1px] border-black rounded-full px-[0.7vw] py-[0.6vh] font-semibold">
               프로필 수정
             </button>
             <button
-              className="text-black border-[1px] border-black rounded-full px-4 py-1 font-semibold"
+              className="text-black border-[1px] border-black rounded-full px-[0.7vw] py-[0.6vh] font-semibold"
               onClick={writePage}
             >
               + 글쓰기
@@ -68,14 +71,17 @@ const Profile = () => {
       } else {
         // 로그인 상태이지만 다른 사람의 프로필인 경우
         return (
-          <button className="text-black border-[1px] border-black rounded-full px-4 py-1 font-semibold">
+          <button className="text-black border-[1px] border-black rounded-full px-[0.5vw] py-[0.5vh] font-semibold">
             팔로우
           </button>
         );
       }
     }
     return (
-      <button className="text-black border-[1px] border-black rounded-full px-4 py-1 font-semibold" onClick={goLoginPage} >
+      <button
+        className="text-black border-[1px] border-black rounded-full px-[0.5vw] py-[0.5vh] font-semibold"
+        onClick={goLoginPage}
+      >
         팔로우
       </button>
     ); // 인증되지 않은 경우 아무 버튼도 표시하지 않음
@@ -83,40 +89,49 @@ const Profile = () => {
 
   // 프로필 정보를 렌더링
   return (
+    <div className="w-[11.3vw] h-[55vh] px-[0.5vw] pt-[2vh] flex flex-col justify-start items-center overflow-hidden">
+      <img
+        src={profileData.profileImage}
+        alt="프로필사진"
+        className="w-[11vw] h-[18vh] rounded-full"
+      />
+      <div className="w-[10vw] h-[3.5vh] text-[22px] mt-[2vh] text-gray-600 font-semibold overflow-hidden">
+        {profileData.nickname}
+      </div>
+      <div className="mt-[0.5vh] text-blue-600 font-semibold">
+        {profileData.mbti}
+      </div>
+      <div className="w-[10vw] h-[3vh]  overflow-hidden">
+        {profileData.mbtiDescription}
+      </div>
+      <div
+        className={`w-[10vw] h-[6vh] mt-[1vh] break-words text-left overflow-hidden ${
+          profileData.profileDescription &&
+          profileData.profileDescription.trim() !== ""
+            ? "text-gray-600"
+            : "text-gray-400 flex items-center justify-center"
+        }`}
+      >
+        {profileData.profileDescription &&
+        profileData.profileDescription.trim() !== ""
+          ? profileData.profileDescription
+          : "상태메시지 없음"}
+      </div>
 
-      <>
-        <div className="flex w-[100%] h-[25%] justify-center items-center overflow-hidden">
-          <img
-            src={profileData.profileImage}
-            alt="프로필사진"
-            className="w-[175px] h-[175px] rounded-full"
-          />
+      <div className="flex space-x-[1vw] mt-[1vh]">
+        <div>
+          <div className="font-semibold">팔로워</div>
+          <div className="text-blue-600">{profileData.followerCount}</div>
         </div>
-        <div className="mt-3 text-[20px] font-semibold">
-          {profileData.nickname}
+        <div>
+          <div className="font-semibold">팔로잉</div>
+          <div className="text-blue-600">{profileData.followingCount}</div>
         </div>
-        <div className="mt-1 text-blue-600 font-semibold">
-          {profileData.mbti}
-        </div>
-        <div className="mt-1">{profileData.mbtiDescription}</div>
-        <div className="mt-3 text-gray-600">
-          {profileData.profileDescription}
-        </div>
-        <div className="flex justify-around mt-4">
-          <div>
-            <div className="font-semibold">팔로워</div>
-            <div className="text-blue-600">{profileData.followerCount}</div>
-          </div>
-          <div>
-            <div className="font-semibold">팔로잉</div>
-            <div className="text-blue-600">{profileData.followingCount}</div>
-          </div>
-        </div>
-        <div className="w-[100%] h-[40px] flex justify-center items-center mt-2 space-x-1.5 overflow-hidden">
-          {renderButtons()}
-        </div>
-
-      </>
+      </div>
+      <div className="w-[11vw] h-[10vh] mt-[-1vh] flex justify-center items-center space-x-[0.3vw] overflow-hidden">
+        {renderButtons()}
+      </div>
+    </div>
   );
 };
 
